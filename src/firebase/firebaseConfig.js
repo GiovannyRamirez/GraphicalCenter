ff0526b90d142ce8d8c3d0a4009652f714d79c8d
GiovannyRamirez <62915328+GiovannyRamirez@users.noreply.github.com>
2021-02-13 22:09:21 -0500

    Add env variables into firebaseConfig (#12)

--- src/firebase/firebaseConfig.js
+++ src/firebase/firebaseConfig.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyANSGGYSd1d3eOq6D1-vwGSf1kjrsrFGWI",
-  authDomain: "graphical-center.firebaseapp.com",
-  projectId: "graphical-center",
-  storageBucket: "graphical-center.appspot.com",
-  messagingSenderId: "1044628058768",
-  appId: "1:1044628058768:web:e77bb59021a7cdd6599e15"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
 }
 
 firebase.initializeApp(firebaseConfig)
